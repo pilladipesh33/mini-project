7627323ce15decfd6adb0e8e2acec4908a67fe02
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-02-15 19:10:05 +0530

    page styling

--- app/(master)/random-color-generator/page.tsx
+++ app/(master)/random-color-generator/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './style.scss';
+
 function RandomColorGenerator() {
-  return <div>RandomColorGenerator</div>;
+  return (
+    <div>
+      <h1 className='title'>Random Color Generator</h1>
+    </div>
+  );
 }
 
 export default RandomColorGenerator;
