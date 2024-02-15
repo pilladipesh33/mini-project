aa2e5da1d77bc7d4dd6b9ec21fa7d7c4ef8808cb
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-02-15 19:05:47 +0530

    accordion added

---
+++ app/(master)/accordion/data.ts
@@ -0,0 +1,22 @@
+export const accordionData = [
+  {
+    title: 'What are accordion components?',
+    content:
+      'Accordion components are user interface elements used for organizing and presenting content in a collapsible manner. They typically consist of a header, content, and an expand/collapse action.',
+  },
+  {
+    title: 'What are they used for?',
+    content:
+      'They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.',
+  },
+  {
+    title: 'Accordion as a musical instrument',
+    content:
+      'The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music genres.',
+  },
+  {
+    title: 'Can I create an accordion component with a different framework?',
+    content:
+      'Yes of course, it is very possible to create an accordion component with another framework.',
+  },
+];
--- app/(master)/accordion/page.tsx
+++ app/(master)/accordion/page.tsx
@@ -1,5 +1,50 @@
+'use client';
+
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import './style.scss';
+import { accordionData } from '@/app/(master)/accordion/data';
+import { useState } from 'react';
+
+type AccordionComponentProps = {
+  title: string;
+  content: string;
+  key: string;
+  index?: number | null;
+};
+
 const AccordionPage = () => {
-  return <div>Accordion Container</div>;
+  return (
+    <div>
+      <h1 className='title'>Accordion</h1>
+      <div className='container'>
+        <h1 className='sub-text'>Multi-Tab Accordion</h1>
+        {accordionData.map((item) => (
+          <MultiAccordionComponent
+            title={item.title}
+            key={item.title}
+            content={item.content}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AccordionPage;
+
+function MultiAccordionComponent({
+  title,
+  content,
+  key,
+}: AccordionComponentProps) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div className='box' key={key}>
+      <div className='box-container' onClick={() => setIsActive(!isActive)}>
+        <h1>{title}</h1>
+        {isActive == true ? <ChevronUp /> : <ChevronDown />}
+      </div>
+      {isActive && <p className='content'>{content}</p>}
+    </div>
+  );
+}
